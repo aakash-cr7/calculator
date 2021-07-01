925705c8d3da7b177cd4b7b65004958c0036bd3c
Aakash Tyagi <aakash.tyg93@gmail.com>
2021-07-01 15:29:37 +0530

    Change Display panel color to red for viewport width less than 500px

--- src/component/Display.js
+++ src/component/Display.js
@@ -8,9 +8,48 @@ export default class Display extends React.Component {
     value: PropTypes.string,
   };
 
+  state = {
+    window: { height: null, width: null },
+  };
+
+  getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  };
+
+  handleResize = () => {
+    this.setState({
+      window: this.getWindowDimensions(),
+    });
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
+  getPanelColor() {
+    const {
+      window: { width },
+    } = this.state;
+    const styles = {};
+
+    if (width && width < 500) {
+      styles["background"] = "red";
+    }
+
+    return styles;
+  }
+
   render() {
     return (
-      <div className="component-display">
+      <div className="component-display" style={this.getPanelColor()}>
         <div>{this.props.value}</div>
       </div>
     );
